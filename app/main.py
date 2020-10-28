31babdba09b17e96dee738a86fab3ea9f80b4562
Artem Kostyukov <apkostyukov@gmail.com>
2020-10-28 16:55:11 +0300

    commit 2

--- app/main.py
+++ app/main.py
@@ -1,11 +1,10 @@
 import cgi
+import re
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 from app.config import env, session
-from app.decorators import commit_transaction
 from app.forms import TaskForm
 from app.models import Task
-import re
 
 
 class TaskHandler(BaseHTTPRequestHandler):
@@ -15,68 +14,89 @@ class TaskHandler(BaseHTTPRequestHandler):
         self.end_headers()
 
     def do_GET(self):
-        response = routes(self.path)
+        response = routes_get(self.path)
 
         self.set_headers(*response.headers)
         self.wfile.write(response.data.encode())
 
-    @commit_transaction
     def do_POST(self):
-        self.set_headers(303, 'Location', '/')
-
         data = cgi.FieldStorage(
             fp=self.rfile,
+            headers=self.headers,
             environ={
                 'REQUEST_METHOD': 'POST'
             }
         )
 
-        session.add(Task(data.getvalue('task')))
+        response = routes_post(self.path, data)
+        self.set_headers(*response.headers)
+        self.wfile.write(response.data.encode())
 
 
 class Response:
     headers = ()
     data = ''
 
-    def task_list(self):
-        self.headers = 200, 'Content-Type', 'text/html'
-        self.data = (
-            env.get_template('index.html').render(form=TaskForm(),
-                                                  tasks=session.query(Task).filter_by(status=True).all()[::-1],
-                                                  done_tasks=session.query(Task).filter_by(status=False).all())
-        )
-        return self
-
-    def delete_task(self, task_id):
-        self.headers = 303, 'Location', '/'
-        session.query(Task).get(task_id).delete_task()
-
-        return self
-
-    def done_task(self, task_id):
-        self.headers = 303, 'Location', '/'
-        session.query(Task).get(task_id).set_done()
-
-        return self
-
-    def clear_all(self):
-        self.headers = 303, 'Location', '/'
-        Task.clear_all()
-
-        return self
+    def __init__(self, headers, data=''):
+        self.headers = headers
+        self.data = data
 
 
-def routes(path):
+def task_list():
+    headers = 200, 'Content-Type', 'text/html'
+    data = env.get_template('index.html').render(form=TaskForm(),
+                                                 tasks=session.query(Task).filter_by(status=True).all()[::-1],
+                                                 done_tasks=session.query(Task).filter_by(status=False).all()
+                                                 )
+    return Response(headers, data)
+
+
+def add_task(data):
+    headers = 303, 'Location', '/'
+
+    session.add(Task(data.getvalue('task')))
+    session.commit()
+
+    return Response(headers)
+
+
+def delete_task(task_id):
+    headers = 303, 'Location', '/'
+    session.query(Task).get(task_id).delete_task()
+
+    return Response(headers)
+
+
+def done_task(task_id):
+    headers = 303, 'Location', '/'
+    session.query(Task).get(task_id).set_done()
+
+    return Response(headers)
+
+
+def clear_all():
+    headers = 303, 'Location', '/'
+    Task.clear_all()
+
+    return Response(headers)
+
+
+def routes_get(path):
     task_id = re.findall(r'/\w+/(\d+)', path)
 
     if path.startswith('/delete'):
-        return Response().delete_task(task_id)
+        return delete_task(task_id)
     elif path.startswith('/done'):
-        return Response().done_task(task_id)
+        return done_task(task_id)
     elif path.startswith('/clear'):
-        return Response().clear_all()
+        return clear_all()
     else:
-        return Response().task_list()
+        return task_list()
+
+
+def routes_post(path, data):
+    if path.startswith('/'):
+        return add_task(data)
 
 
 server = HTTPServer(('', 8000), TaskHandler)
